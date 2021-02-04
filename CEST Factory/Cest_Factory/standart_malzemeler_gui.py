75df456afa433b6c9765b043136f237d0047095b
Barış Kılıçlar <bariskiliclar@gmail.com>
2021-02-04 12:10:23 +0200

    Önemsiz

--- CEST Factory/Cest_Factory/standart_malzemeler_gui.py
+++ CEST Factory/Cest_Factory/standart_malzemeler_gui.py
@@ -4,17 +4,26 @@ Created on 30 Oca 2021
 @author: user
 '''
 
-class MyClass(object):
+class standart_malzemeler_view_edit_gui(QDialog):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, anapencere):
         '''
         Constructor
         '''
         super().__init__()
+        self.anapencere = anapencere
+        self.rowNo = index.row()
+
+        #self.bilgileriAl()
+        self.changeTexts()
+        self.dialogUIoluştur()
+        self.dialogMoveCenter()
+        self.fillthefields()
+        self.show()
     def hellorilerdengel(self):
         print("Ule hellorilerden gelirem uleeeooo")
         print("ule ule uleeeee")
